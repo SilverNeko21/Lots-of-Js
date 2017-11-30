65f542636720906817fa3ef4437520dd5493c261
SilverNeko21 <larong2122@gmail.com>
2017-11-29 22:02:44 -1000

    One Done

---
+++ magic.js
@@ -0,0 +1,31 @@
+var hero = "Jake";
+var gender = "male";
+var age = 25;
+var weapon = "sword";
+var pronoun ;
+
+function thePronoun() {
+	if (gender == "male"){
+		pronoun = "he";
+	} else if (gender == "female") {
+		pronoun = "she";
+	}
+}
+thePronoun();
+
+var output ;
+function heroAge() {
+	if (age < 18) {
+		output = "is just beginning";
+	} else if (age >= 21 && age <= 40) {
+		output = "has seen a few battles";
+	} else if (age > 40) {
+		output = "has lived through many epic battles";
+	}
+}
+heroAge();
+
+function theHero() {
+	return hero + " is our lovable hero, who " + output + " with his trusty " + weapon + "."
+}
+console.log(theHero())
